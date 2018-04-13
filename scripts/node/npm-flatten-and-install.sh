87ae3dd50e0b78372e07e9e910e82389f6b5c279
Matt Anderson <matt@mattbanderson.com>
2018-04-13 15:17:02 -0400

    Update npm flatten script to clean up after itself.

--- scripts/node/npm-flatten-and-install.sh
+++ scripts/node/npm-flatten-and-install.sh
@@ -1,1 +1,9 @@
-npm ll --parseable | sed 's#\(.*\):\(.*\):\(.*\)#npm install \2#;w ./install-packages.sh' && chmod +x ./install-packages.sh && ./install-packages.sh
+npm ll --parseable | sed 's#\(.*\):\(.*\):\(.*\)#npm install \2#;w ./install-packages.sh'
+mkdir -p ../npm-cache-mirror
+mv ./install-packages.sh ../npm-cache-mirror
+cd ../npm-cache-mirror
+npm cache clean --force
+chmod +x ./install-packages.sh 
+./install-packages.sh
+cd ../
+rm -rf npm-cache-mirror
